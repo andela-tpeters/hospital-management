1d96848476e0e611bf7731fe5ab887826c592fa6
Tijesunimi Peters <tije@starcityhq.zerofoursixty.com>
2016-03-24 11:36:05 +0100

    set the normal on the model

--- app/AccountModel.php
+++ app/AccountModel.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class AccountModel extends Model
 {
-    
+    protected $table = 'account';
+
+    protected $primaryKey = 'patient_id';
+
+    Protected $fillable = ['amount','purpose'];
 }
